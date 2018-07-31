32173ee55ac10665f3374fb2e5ff7d642cc05ba4
David Lucchesi <lucchesi.elype@gmail.com>
2018-07-31 18:10:55 +0200

    Gestion des images

--- Components/FilmItem.js
+++ Components/FilmItem.js
@@ -1,31 +1,33 @@
 // Components/FilmItem.js
 
+import { getImageFromApi } from '../API/TMDBApi'
 import React from 'react'
 import { StyleSheet, View, Text, Image } from 'react-native'
 
 class FilmItem extends React.Component {
   render() {
-      const film = this.props.film
-      return (
-        <View style={styles.main_container}>
-          <Image
-            style={styles.image}
-            source={{uri: "image"}}
-          />
-          <View style={styles.content_container}>
-            <View style={styles.header_container}>
-              <Text style={styles.title_text}>{film.title}</Text>
-              <Text style={styles.vote_text}>{film.vote_average}</Text>
-            </View>
-            <View style={styles.description_container}>
-              <Text style={styles.description_text} numberOfLines={6}>{film.overview}</Text>
-            </View>
-            <View style={styles.date_container}>
-              <Text style={styles.date_text}>Sorti le {film.release_date}</Text>
-            </View>
-          </View>
-        </View>
-      )
+    const film = this.props.film
+    return (
+      <View style={styles.main_container}>
+      <Image
+      style={styles.image}
+      source={{uri: getImageFromApi(film.poster_path)}}
+
+      />
+      <View style={styles.content_container}>
+      <View style={styles.header_container}>
+      <Text style={styles.title_text}>{film.title}</Text>
+      <Text style={styles.vote_text}>{film.vote_average}</Text>
+      </View>
+      <View style={styles.description_container}>
+      <Text style={styles.description_text} numberOfLines={6}>{film.overview}</Text>
+      </View>
+      <View style={styles.date_container}>
+      <Text style={styles.date_text}>Sorti le {film.release_date}</Text>
+      </View>
+      </View>
+      </View>
+    )
   }
 }
 
@@ -37,8 +39,7 @@ const styles = StyleSheet.create({
   image: {
     width: 120,
     height: 180,
-    margin: 5,
-    backgroundColor: 'gray'
+    margin: 5
   },
   content_container: {
     flex: 1,
